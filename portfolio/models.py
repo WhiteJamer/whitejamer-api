036263985fd52d35536223bd503201bb5f2066d1
whitejamer <muxah3258@gmail.com>
2020-01-04 18:31:55 +0300

    Add owner_field to PortfolioItem

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from profiles.models import Profile
 
 
 class PortfolioItem(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     img = models.FileField(upload_to='portfolio/', null=True, blank=True)
+    owner = models.ForeignKey(Profile, related_name='items', null=True, on_delete=models.CASCADE)
 
-    def str(self):
+    def __str__(self):
         return self.title
