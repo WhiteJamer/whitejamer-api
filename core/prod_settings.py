2edee1ceec2158815c80843eb6ff95bf203306e8
WhiteJamer <33582148+WhiteJamer@users.noreply.github.com>
2020-01-06 17:38:57 +0300

    Switch DB

--- core/prod_settings.py
+++ core/prod_settings.py
@@ -8,11 +8,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '8on$iujotn1zl^5t1+_elot62^y(wl
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USERNAME'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '../my.cnf',
+        },
     }
 }
