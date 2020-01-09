6eeb5a312607ab461274d5afda5c0a1527b19242
whitejamer <muxah3258@gmail.com>
2020-01-09 23:35:27 +0300

    Integrade TinyMCE in Admin-Interface

--- core/settings.py
+++ core/settings.py
@@ -119,8 +119,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 AUTH_USER_MODEL = 'profiles.Profile'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import PortfolioItem
 
-admin.site.register(PortfolioItem)
+
+class PortfolioAdmin(admin.ModelAdmin):
+    class Media:
+        js = (
+            'https://cdn.tiny.cloud/1/b9k3a3np6zu1ev60uccs4sveuh41wrrftes6etizdpts3sod/tinymce/5/tinymce.min.js',
+            'js/tiny_init.js'
+        )
+
+
+admin.site.register(PortfolioItem, PortfolioAdmin)
